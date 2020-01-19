8e70bc6786dab9f10f41570ce73d34c1dff3e450
David Kislyak <dkislyak@mail.greenriver.edu>
2020-01-19 15:46:37 -0500

    Add composer file and f3 routing to index.php.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    /*
+     * David Kislyak
+     * 01/15/2020
+     * /328/dating/index.php
+     */
+
+    // Error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    //Require autoload file
+    require("./vendor/autoload.php");
+
+    //Instantiate F3
+    $f3 = Base::instance();
+
+    //Define a default route
+    $f3->route('GET /', function() {
+        $view = new Template();
+        echo $view->render('./views/home.html');
+    });
+
+    //Run fat free
+    $f3->run();
